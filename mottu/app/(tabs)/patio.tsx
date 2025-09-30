7ded254bf88627c1130d2c42cff7d657e3a28646
tcguus <gugucram15@gmail.com>
2025-09-30 17:13:33 -0300

    Implementando a funcionalidade de tema claro e escuro na tela: Patio.

--- mottu/app/(tabs)/patio.tsx
+++ mottu/app/(tabs)/patio.tsx
@@ -15,7 +15,8 @@ import { Ionicons } from "@expo/vector-icons";
 import Header from "../../components/Header";
 import { useFocusEffect } from "@react-navigation/native";
 import api from "../../services/api";
-import { rawColors } from "@/constants/theme";
+import { rawColors } from "../../constants/theme";
+import { useTheme } from "../../context/ThemeContext";
 
 type MotoApi = {
   placa: string;
@@ -38,6 +39,7 @@ type MotoCompleta = MotoApi & {
 const CHASSI_STORAGE_KEY = "@motos_chassi";
 
 export default function Patio() {
+  const { colors } = useTheme();
   const [motos, setMotos] = useState<MotoCompleta[]>([]);
   const [selectedMoto, setSelectedMoto] = useState<MotoCompleta | null>(null);
   const [modalVisible, setModalVisible] = useState(false);
@@ -158,7 +160,7 @@ export default function Patio() {
   };
 
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, { backgroundColor: colors.background }]}>
       <Header title="Pátio Digital" showBackButton={true} />
 
       <DropDownPicker
@@ -187,10 +189,43 @@ export default function Patio() {
         setItems={setDropdownItems}
         placeholder="Pesquise por placa"
         dropDownDirection="BOTTOM"
-        style={styles.dropdown}
-        dropDownContainerStyle={styles.dropdownList}
+        style={[styles.dropdown, { backgroundColor: colors.background }]}
+        dropDownContainerStyle={[
+          styles.dropdownList,
+          { backgroundColor: colors.background },
+        ]}
         zIndex={10}
         zIndexInverse={9}
+        textStyle={{
+          color: colors.text,
+        }}
+        placeholderStyle={{
+          color: colors.text,
+        }}
+        ArrowDownIconComponent={({ style }) => (
+          <Ionicons
+            name="chevron-down"
+            size={20}
+            color={colors.text}
+            styles={style}
+          />
+        )}
+        ArrowUpIconComponent={({ style }) => (
+          <Ionicons
+            name="chevron-up"
+            size={20}
+            color={colors.text}
+            styles={style}
+          />
+        )}
+        TickIconComponent={({ style }) => (
+          <Ionicons
+            name="checkmark"
+            size={20}
+            color={colors.text}
+            styles={style}
+          />
+        )}
       />
 
       <FlatList
@@ -208,24 +243,39 @@ export default function Patio() {
 
       <Modal visible={modalVisible} transparent animationType="slide">
         <View style={styles.modalContainer}>
-          <View style={styles.modalContent}>
+          <View
+            style={[
+              styles.modalContent,
+              { backgroundColor: colors.background },
+            ]}
+          >
             <TouchableOpacity onPress={fecharModal} style={styles.closeIcon}>
               <Ionicons name="close" size={28} color="#999" />
             </TouchableOpacity>
-            <View style={styles.info}>
+            <View style={[styles.info, { backgroundColor: colors.background }]}>
               <View style={styles.desc}>
-                <Text style={styles.labelInfo}>Placa e modelo:</Text>
-                <Text style={styles.inputInfo}>
+                <Text style={[styles.labelInfo, { color: colors.text }]}>
+                  Placa e modelo:
+                </Text>
+                <Text style={[styles.inputInfo, { color: colors.subtext }]}>
                   {selectedMoto?.placa} | {selectedMoto?.modelo}
                 </Text>
               </View>
               <View style={styles.desc}>
-                <Text style={styles.labelInfo}>Ano:</Text>
-                <Text style={styles.inputInfo}>{selectedMoto?.ano}</Text>
+                <Text style={[styles.labelInfo, { color: colors.text }]}>
+                  Ano:
+                </Text>
+                <Text style={[styles.inputInfo, { color: colors.subtext }]}>
+                  {selectedMoto?.ano}
+                </Text>
               </View>
               <View style={styles.desc}>
-                <Text style={styles.labelInfo}>Chassi (VIN):</Text>
-                <Text style={styles.inputInfo}>{selectedMoto?.chassi}</Text>
+                <Text style={[styles.labelInfo, { color: colors.text }]}>
+                  Chassi (VIN):
+                </Text>
+                <Text style={[styles.inputInfo, { color: colors.subtext }]}>
+                  {selectedMoto?.chassi}
+                </Text>
               </View>
             </View>
 
@@ -275,9 +325,10 @@ const styles = StyleSheet.create({
     alignSelf: "center",
   },
   dropdownList: {
-    borderColor: "#888",
+    borderColor: "#00B131",
+    borderTopWidth: 0,
     marginHorizontal: 20,
-    borderWidth: 1,
+    borderWidth: 2,
     width: "90%",
     alignSelf: "center",
   },
